79ec5a74d9a3bfb6fb9f4c78dbe52c0ac1ac0e35
mukesh.kumar <mukesh.kumar@decimal.co.in>
2020-01-27 13:23:09 +0530

    angular material module added

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -1,21 +1,67 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatFormFieldModule,
-  MatInputModule
-} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {} from '@angular/common';
 
+import { 
+  MatButtonModule,
+  MatCheckboxModule,
+  MatSlideToggleModule,
+  MatSliderModule,
+  MatRadioModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatCardModule,
+  MatListModule,
+  MatIconModule,
+  MatDividerModule,
+  MatMenuModule,
+  MatTableModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatRadioModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatRadioModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
